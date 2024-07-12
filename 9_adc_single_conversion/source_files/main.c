65e43d2293a6c7101af69b6718cd50fd71996e25
Wise-deldu <delduwise@gmail.com>
2024-07-12 05:19:03 +0000

    A program that demonstrates the adc module

---
+++ 9_adc_single_conversion/source_files/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "uart.h"
+#include "adc.h"
+
+
+uint32_t sensor_value;
+
+int main(void)
+{
+
+    uart1_tx_init(); /* Initialized the tx */
+    pa1_adc_init(); /* Initialized adc */
+
+    while (1)
+    {
+        start_conversion();
+
+    	sensor_value = adc_read();
+    	printf("Sensor value : %d \n\r", (int)sensor_value);
+    }
+}
